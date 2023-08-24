7897ebadcd76f2cdbdbdbe6d6c3073dac2518e4e
Kumari Ranjana Yadav <yranjana757@gmail.com>
2023-08-24 11:38:32 +0530

    first commit

---
+++ Color_Flipper/app.js
@@ -0,0 +1,28 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025" , "cyan" , "yellow" , "brown" , "pink"];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', () => {
+    // getRandom Number Between 0 - 3
+    const randomNumber = getRandomNumber();
+    // console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
